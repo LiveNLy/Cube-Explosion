3df2642af42a838167ed3cd1c1de15f7cd145f19
LiveNLy <newrivengod@gmail.com>
2024-06-25 23:10:07 +1000

    hm

--- scripts/Cube.cs
+++ scripts/Cube.cs
@@ -1,30 +1,32 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Renderer))]
 public class Cube : MonoBehaviour
 {
     [SerializeField] private int _chanceOfCopy = 100;
     [SerializeField] private Divider _divider;
     [SerializeField] private Exploder _exploder;
+    [SerializeField] private Cube _cube;
 
     private int _chanceReductionPercentage = 2;
 
     private void OnMouseUpAsButton()
-    {
-        TakeAction();
-    }
-
-    private void TakeAction()
     {
         int chanceOfCloning = Random.Range(0, 101);
 
         if (chanceOfCloning <= _chanceOfCopy)
         {
             _chanceOfCopy /= _chanceReductionPercentage;
-            _divider.Divide();
+            _divider.Divide(_cube);
         }
         else
         {
             _exploder.Explode();
         }
+    }
+
+    public void ChangeColor()
+    {
+        _cube.GetComponent<Renderer>().material.color = Random.ColorHSV();
     }
 }
--- scripts/Divider.cs
+++ scripts/Divider.cs
@@ -1,30 +1,23 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Cube))]
 public class Divider : MonoBehaviour
 {
     [SerializeField] private Spawner _spawner;
-    [SerializeField] CollorChanger _collorChanger;
 
-    private Cube _cube;
     private int _cubesCountMax = 6;
     private int _cubesCountMin = 2;
-    private int _cubesCount;
     private int _scaleReducePercent = 2;
 
-    private void Start()
+    public void Divide(Cube cube)
     {
-        _cube = GetComponent<Cube>();
-    }
+        int cubesCount;
+        cube.transform.localScale /= _scaleReducePercent;
+        cubesCount = Random.Range(_cubesCountMin, _cubesCountMax + 1);
 
-    public void Divide()
-    {
-        _cube.transform.localScale /= _scaleReducePercent;
-        _cubesCount = Random.Range(_cubesCountMin, _cubesCountMax);
-
-        for (int i = 0; i < _cubesCount; i++)
+        for (int i = 0; i < cubesCount; i++)
         {
-            _collorChanger.ChangeCollor(_spawner.Spawn(_cube));
+            Cube newCube = _spawner.Spawn(cube);
+            newCube.ChangeColor();
         }
     }
 }
